57f029ff688c2e72ec89dd0931d287a7a9e79f8b
rajeshp1106 <156574497+rajeshp1106@users.noreply.github.com>
2025-04-17 22:57:33 +0530

    Delete frontend directory

--- frontend/ipo-web-app/src/services/mockUsers.js
+++
@@ -1,6 +0,0 @@
-// src/data/mockUsers.js
-export const mockUsers = [
-    { username: "user1", password: "userpass", role: "user" },
-    { username: "admin1", password: "adminpass", role: "admin" }
-  ];
-  
